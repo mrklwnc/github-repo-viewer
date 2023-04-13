543c81afad303228f15bc8032abf183b04697794
HilthEXE <endranomarklewence@gmail.com>
2023-04-14 00:01:20 +0800

    10:36

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About NextJS 13</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nemo
+        nesciunt. Quis deleniti qui dolor fugiat facilis, iusto similique sint
+        fuga nihil, quo saepe inventore consequatur ipsam, error quam eaque!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
